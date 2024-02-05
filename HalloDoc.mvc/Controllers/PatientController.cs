11839393bc440bd10f614d01daa14630b2e6e161
vatsalTatva <vatsal.gadoya@etatvasoft.com>
2024-02-05 18:44:41 +0530

    first commit

---
+++ HalloDoc.mvc/Controllers/PatientController.cs
@@ -0,0 +1,55 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace HalloDoc.mvc.Controllers
+{
+   public class PatientController : Controller
+    {
+
+        private readonly ILogger<PatientController> _logger;
+
+        public PatientController(ILogger<PatientController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        public IActionResult RequestScreen()
+        {
+            return View();
+        }
+
+        public IActionResult CreatePatientReq()
+        {
+            return View();
+        }
+
+        public IActionResult CreateFamilyFrndReq()
+        {
+            return View();
+        }
+
+        public IActionResult CreateConciergeReq()
+        {
+            return View();
+        }
+
+        public IActionResult CreateBusinessPartnerReq()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult ForgetPassword()
+        {
+            return View();
+        }
+
+
+
+    }
+}
---
+++ HalloDoc.mvc/Views/Patient/RequestScreen.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HalloDoc.mvc.Views.Patient
+{
+    public class RequestScreenModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
